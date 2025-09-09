39862361abf4f728b633e380fca5e38e5de2d5b2
IamNishant51 <anonymouslucifer400@gmail.com>
2025-09-09 22:56:17 +0530

    fixed all icons

--- src/extension.ts
+++ src/extension.ts
@@ -2624,6 +2624,7 @@ function getProfileWebviewContent(webview: vscode.Webview, userData: any, reposi
 
                 .repo {
                     display: flex;
+                    /* align icon with first line of repo name */
                     align-items: flex-start;
                     width: 100%;
                     text-align: left;
@@ -2631,10 +2632,12 @@ function getProfileWebviewContent(webview: vscode.Webview, userData: any, reposi
 
                 .repo-icon {
                     margin-right: 12px;
-                    margin-top: 2px;
+                    /* adjusted top margin for better baseline alignment with repo name */
+                    margin-top: 5px;
                     fill: var(--color-fg-muted);
                     flex-shrink: 0;
                 }
+                .starred-repo > .repo-icon { margin-top: 5px; }
 
                 .repo-info {
                     min-width: 0;
@@ -2779,7 +2782,7 @@ function getProfileWebviewContent(webview: vscode.Webview, userData: any, reposi
 
                 .starred-repo {
                     display: flex;
-                    align-items: flex-start;
+                    align-items: flex-start; /* baseline alignment with name */
                     width: 100%;
                 }
 
@@ -3614,7 +3617,7 @@ function getProfileWebviewContent(webview: vscode.Webview, userData: any, reposi
                         <p class="profile-login">${userData.login}</p>
                         
                         <button id="createRepoBtn" class="btn-primary">
-                            <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor" style="margin-right: 8px;">
+                            <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor" style="margin-right:8px; display:inline-block; vertical-align:middle;">
                                 <path d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"></path>
                             </svg>
                             New repository
